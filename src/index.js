36d20bed3600084fbedb318d428db573a546e385
Davi Duarte Gomes <daviduartebrancol@gmail.com>
2020-07-25 12:38:49 -0300

    Created TextLocalizeProvider and useTextLocalize context functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
-import React from "react";
+import React, { useState, createContext, useEffect } from "react";
 
-export function init() {
-  return <div></div>;
+const TextLocalizeContext = createContext(null);
+
+export function TextLocalizeProvider(props) {
+  const [dictionary, setDictionary] = useState(null);
+
+  async function mountProvider() {
+    let language = await import("../../../src/lang/en-us.json");
+    setDictionary(language);
+  }
+
+  useEffect(() => {
+    mountProvider();
+  }, []);
+
+  function localize(id) {
+    console.log(dictionary);
+    return dictionary ? dictionary[id] : null;
+  }
+
+  return (
+    <TextLocalizeContext.Provider
+      value={{
+        localize,
+      }}
+      {...props}
+    />
+  );
+}
+
+export function useTextLocalize() {
+  const context = React.useContext(TextLocalizeContext);
+  if (context === undefined) {
+    throw new Error(
+      `useTextLocalize must be used within a TextLocalizeProvider`
+    );
+  }
+  return context;
 }
